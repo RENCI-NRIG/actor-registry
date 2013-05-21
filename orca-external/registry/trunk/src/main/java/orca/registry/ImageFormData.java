50c641fb22537fab820594fbc94ba5e59f90f812
ibaldin <ibaldin@9b6b0240-9c15-4950-99ad-f55ef948cf6a>
2013-05-21 04:25:24 +0000

    Mostly working

--- orca-external/registry/trunk/src/main/java/orca/registry/ImageFormData.java
+++ orca-external/registry/trunk/src/main/java/orca/registry/ImageFormData.java
@@ -1,14 +1,14 @@
 package orca.registry;
 
 public class ImageFormData {
-	String guid, action;
+	String hash, action, name, url, owner, ver, nver, desc;
 	
 	public void setHash(String g) {
-		guid = g;
+		hash = g;
 	}
 	
 	public String getHash() {
-		return guid;
+		return hash;
 	}
 	
 	public void setAction(String a) {
@@ -17,5 +17,53 @@ public class ImageFormData {
 	
 	public String getAction() {
 		return action;
+	}
+	
+	public void setName(String n) {
+		name = n;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setUrl(String u){
+		url = u;
+	}
+	
+	public String getUrl() {
+		return url;
+	}
+	
+	public void setOwner(String o) {
+		owner = o;
+	}
+	
+	public String getOwner() {
+		return owner;
+	}
+	
+	public void setVer(String v) {
+		ver = v;
+	}
+	
+	public String getVer() {
+		return ver;
+	}
+	
+	public void setNver(String n) {
+		nver = n;
+	}
+
+	public String getNver() {
+		return nver;
+	}
+	
+	public void setDesc(String d) {
+		desc = d;
+	}
+	
+	public String getDesc() {
+		return desc;
 	}
 }
--- orca-external/registry/trunk/src/main/webapp/secure/image-action.jsp
+++ orca-external/registry/trunk/src/main/webapp/secure/image-action.jsp
@@ -5,6 +5,8 @@
 
 DatabaseOperations dbop = new DatabaseOperations();
 
+System.out.println("Dealing with request " + data.getAction());
+
 if (data.getHash() != null) {
 	if (data.getAction().equals("delete")) {
 		dbop.deleteImage(org.apache.commons.lang3.StringEscapeUtils.unescapeEcmaScript(data.getHash()));
@@ -20,6 +22,14 @@ if (data.getHash() != null) {
 <script language="javascript" type="text/javascript">
 	window.location="../images.jsp";
 </script>
+<%
+    } else if (data.getAction().equals("add")) {
+    	System.out.println("Adding image " + data.getName() + " " + data.getUrl());
+        dbop.insertImage(data.getName(), data.getVer(), data.getNver(), data.getUrl(), data.getHash(), data.getOwner(), data.getDesc(), false);
+%>
+  <script language="javascript" type="text/javascript">
+	window.location="../images.jsp";
+</script>     
 <%
 	} else {
 %>
